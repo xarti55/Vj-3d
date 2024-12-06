9a9195ebc48a37caab0d5787ad244a301787d649
xavier lopez <xavier.lopez.manes@estudiantat.upc.edu>
2024-12-06 22:50:12 +0100

    a

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 
 public struct map_position_properties
 {
     public float height;
-    public int next_direction; //0 = left, 1 = top, 2 = right, 3 = bottom
-    public int old_direction; //0 = left, 1 = top, 2 = right, 3 = bottom
+    public int next_direction; //-1 = left, -2 = bottom, 1 = right, 2 = top
+    public int old_direction; //-1 = left, -2 = bottom, 1 = right, 2 = top
 } 
 
 public struct lvl_position
 {
     public int pos_x;
-    public int pos_y;
+    public int pos_z;
     public float height;
 }
 
@@ -22,6 +23,9 @@ public class Map : MonoBehaviour
     private map_position_properties [,] map_positions;
     public static Map Instance; // Singleton Instance
     private lvl_position [,] lv1;
+    private List <GameObject> cubes;
+    int current_stage, n_stages, cube_done, n_cubes;
+    int from_to = 0; // 0 = from, 1 = to
     
 
     private void Awake()
@@ -34,56 +38,99 @@ public class Map : MonoBehaviour
         { 
             Instance = this; 
         }
+        cubes = new List<GameObject>();
         map_positions = new map_position_properties [10,10];
         lv1 = new lvl_position[,]{{
-            new lvl_position { pos_x = 0, pos_y = 0, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 0, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 1, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 2, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 3, height = 0.0f },
-            new lvl_position { pos_x = 2, pos_y = 3, height = 0.0f },
-            new lvl_position { pos_x = 3, pos_y = 3, height = 0.0f },
-            new lvl_position { pos_x = 4, pos_y = 3, height = 0.0f },
-            new lvl_position { pos_x = 4, pos_y = 4, height = 0.0f },
-            new lvl_position { pos_x = 4, pos_y = 5, height = 0.0f },
-            new lvl_position { pos_x = 3, pos_y = 5, height = 0.0f },
-            new lvl_position { pos_x = 2, pos_y = 5, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 5, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 6, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 7, height = 0.0f },
-            new lvl_position { pos_x = 1, pos_y = 8, height = 0.0f },
-            new lvl_position { pos_x = 2, pos_y = 8, height = 0.0f },
-            new lvl_position { pos_x = 3, pos_y = 8, height = 0.0f },
-            new lvl_position { pos_x = 5, pos_y = 8, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 8, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 7, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 6, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 5, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 4, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 3, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 2, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 1, height = 0.0f },
-            new lvl_position { pos_x = 6, pos_y = 0, height = 0.0f }}};
+            new lvl_position { pos_x = 0, pos_z = 0, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 0, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 1, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 2, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 3, height = 1.0f },
+            new lvl_position { pos_x = 2, pos_z = 3, height = 1.0f },
+            new lvl_position { pos_x = 3, pos_z = 3, height = 1.0f },
+            new lvl_position { pos_x = 4, pos_z = 3, height = 1.0f },
+            new lvl_position { pos_x = 4, pos_z = 4, height = 1.0f },
+            new lvl_position { pos_x = 4, pos_z = 5, height = 1.0f },
+            new lvl_position { pos_x = 3, pos_z = 5, height = 2.0f },
+            new lvl_position { pos_x = 2, pos_z = 5, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 5, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 6, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 7, height = 1.0f },
+            new lvl_position { pos_x = 1, pos_z = 8, height = 1.0f },
+            new lvl_position { pos_x = 2, pos_z = 8, height = 1.0f },
+            new lvl_position { pos_x = 3, pos_z = 8, height = 1.0f },
+            new lvl_position { pos_x = 4, pos_z = 8, height = 1.0f },
+            new lvl_position { pos_x = 5, pos_z = 8, height = 1.0f },
+            new lvl_position { pos_x = 6, pos_z = 8, height = 1.0f },
+            new lvl_position { pos_x = 6, pos_z = 7, height = 1.0f },
+            new lvl_position { pos_x = 6, pos_z = 6, height = 2.0f },
+            new lvl_position { pos_x = 6, pos_z = 5, height = 2.0f },
+            new lvl_position { pos_x = 6, pos_z = 4, height = 3.0f },
+            new lvl_position { pos_x = 6, pos_z = 3, height = 1.0f },
+            new lvl_position { pos_x = 6, pos_z = 2, height = 1.0f },
+            new lvl_position { pos_x = 6, pos_z = 1, height = 1.0f },
+            new lvl_position { pos_x = 6, pos_z = 0, height = 1.0f }}};
     }
-
     public bool player_hit(Vector3 player_position)
     {
-       return player_position.z <  map_positions[(int)player_position.x,(int)player_position.y].height - 0.2; 
+       return player_position.z <  (map_positions[(int)player_position.x,(int)player_position.y].height - 0.2); 
     }
 
-    public void load_lvl_stage(int level, int lvl_stage){
-        if (level == 1){
-            for (int i = 0; i <  map_positions[0].Length; ++i){
-                return;
-            }
+    private void add_cube_to_position(lvl_position pos){
+        
+        for (int i = 0; i < pos.height; ++i){
+            cubes.Add(GameObject.CreatePrimitive(PrimitiveType.Cube));
+            cubes[cubes.Count - 1].transform.position = new Vector3(pos.pos_x,i, pos.pos_z);
         }
+        
+    }
+
+    public void load_lvl_stage(int level, int lvl_stage){        
+         if (level == 1){
+            current_stage = lvl_stage;
+            n_stages = lv1.GetLength(0);
+            cube_done = 0;
+            n_cubes = 0;
+            add_cube_to_position(lv1[lvl_stage, 0]);
+            int last_pos = lv1.GetLength(lvl_stage + 1) - 1;
+            map_positions[lvl_stage,0].height = lv1[lvl_stage,0].height;
+            map_positions[lvl_stage,0].old_direction = 2;
+            map_positions[lvl_stage,0].next_direction = (lv1[lvl_stage,0].pos_x != lv1[lvl_stage,1].pos_x) ?  lv1[lvl_stage,1].pos_x - lv1[lvl_stage,0].pos_x   : (lv1[lvl_stage,1].pos_z - lv1[lvl_stage,0].pos_z)*2;
+            ++n_cubes;
+            for (int i = 1; i <  lv1.GetLength(lvl_stage + 1) - 1; ++i){
+                add_cube_to_position(lv1[lvl_stage,i]);
+                map_positions[lv1[lvl_stage,i].pos_x,lv1[lvl_stage,i].pos_z].old_direction = (lv1[lvl_stage,i].pos_x != lv1[lvl_stage,i - 1].pos_x) ?  lv1[lvl_stage,i].pos_x - lv1[lvl_stage,i - 1].pos_x   : (lv1[lvl_stage,i].pos_z - lv1[lvl_stage,i - 1].pos_z)*2;
+                map_positions[lv1[lvl_stage,i].pos_x,lv1[lvl_stage,i].pos_z].next_direction = (lv1[lvl_stage,i].pos_x != lv1[lvl_stage,i + 1].pos_x) ?  lv1[lvl_stage,i + 1].pos_x - lv1[lvl_stage,i].pos_x   : (lv1[lvl_stage,i + 1].pos_z - lv1[lvl_stage,i].pos_z)*2;
+                map_positions[lv1[lvl_stage,i].pos_x,lv1[lvl_stage,i].pos_z].height = lv1[lvl_stage,i].height;
+                ++n_cubes;
+            }
+            add_cube_to_position(lv1[lvl_stage,last_pos]);
+            map_positions[lv1[lvl_stage,last_pos].pos_x,lv1[lvl_stage,last_pos].pos_z].old_direction = (lv1[lvl_stage,last_pos].pos_x != lv1[lvl_stage,last_pos - 1].pos_x) ? lv1[lvl_stage,last_pos].pos_x - lv1[lvl_stage,last_pos - 1].pos_x   : (lv1[lvl_stage,last_pos].pos_z - lv1[lvl_stage,last_pos - 1].pos_z)*2;
+            map_positions[lv1[lvl_stage,last_pos].pos_x,lv1[lvl_stage,last_pos].pos_z].next_direction =  (lv1[lvl_stage,last_pos].pos_x != lv1[lvl_stage,last_pos - 1].pos_x) ? lv1[lvl_stage,last_pos].pos_x - lv1[lvl_stage,last_pos - 1].pos_x   : (lv1[lvl_stage,last_pos].pos_z - lv1[lvl_stage,last_pos - 1].pos_z)*2;
+            map_positions[lv1[lvl_stage,last_pos].pos_x,lv1[lvl_stage,last_pos].pos_z].height = lv1[lvl_stage,last_pos].height;
+            ++n_cubes;
+         }
     } 
 
     public int player_next_move(Vector3 player_position)
     {
-        int old_direction = map_positions[(int)player_position.x,(int)player_position.y].old_direction;
-        bool moving_from = ((old_direction == 0) && (player_position.x - (int)player_position.x <= 0.5)) || ((old_direction == 2) && (player_position.x - (int)player_position.x >= 0.5)) || ((old_direction == 1) && (player_position.y - (int)player_position.y <= 0.5)) || ((old_direction == 4) && (player_position.y - (int)player_position.y >= 0.5));
-        if (moving_from) return map_positions[(int)player_position.x,(int)player_position.y].next_direction;
-        else return  map_positions[(int)player_position.x,(int)player_position.y].old_direction;
+        if (player_position.x > 10 || player_position.x < 0 || player_position.z > 10 || player_position.z < 0 ) return 0; //We've finished the stage
+        int old_direction = map_positions[(int)player_position.x,(int)player_position.z].old_direction;
+        bool moving_from = ((old_direction == -2) && (player_position.z - (float)Math.Floor(player_position.z) >= 0.5)) || ((old_direction == 2) && (player_position.z - (float)Math.Floor(player_position.z) <= 0.5)) || ((old_direction == 1) && (player_position.x - (float)Math.Floor(player_position.x) <= 0.5)) || ((old_direction == -1) && (player_position.x - (float)Math.Floor(player_position.x) >= 0.5));
+        if (moving_from) {
+            from_to = 0;
+            return map_positions[(int)player_position.x,(int)player_position.z].next_direction;
+        }
+        else{
+            if (from_to == 0)  ++cube_done;          
+            from_to = 1;
+            return  map_positions[(int)player_position.x,(int)player_position.z].old_direction;
+        } 
     }
+
+    public int get_percentage(){
+        return (int)(100.0f * ((float)current_stage / (float)n_stages) + (100.0f / (float)n_stages) * ((float)cube_done / (float)n_cubes));
+    }
+
+    
 }
--- Assets/Player_Script.cs
+++ Assets/Player_Script.cs
@@ -2,21 +2,32 @@ using UnityEngine;
 
 public class Player_Script : MonoBehaviour
 {
-    public float movement_speed = 4.0f;
+    public float movement_speed = 0.01f;
 
     private Map my_map;
+    Rigidbody rb;
 
     void Start()
     {
         my_map = Map.Instance; 
+        my_map.load_lvl_stage(1,0);
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+        rb.MovePosition(new Vector3(0,1,0));
     }
 
     void Update()
     {
         // Basic movement logic
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontal, vertical, 0) * Time.deltaTime * movement_speed);
-        Debug.Log(transform.position);
+        float horizontal = 0; 
+        float vertical = 0; 
+        int next_dir = my_map.player_next_move(rb.position);
+        if (next_dir == -1) horizontal = -1;
+        else if (next_dir == 1) horizontal = 1;
+        else if (next_dir == -2) vertical = -1;
+        else if (next_dir == 2) vertical = 1;
+        Vector3 movement = new Vector3(horizontal, 0 , vertical ) * movement_speed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
     }
 }
